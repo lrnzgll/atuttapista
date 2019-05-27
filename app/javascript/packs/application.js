a977c68b16e7e2258db8efe849fcaa4a32c1e518
lorenzo galli <lrnzgll@gmail.com>
2019-05-27 19:32:05 +0100

    third

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,5 @@
-var Turbolinks = require("turbolinks")
-Turbolinks.start()
-
 import "bootstrap";
 import '../stylesheets/application'
 require.context('../stylesheets/images', true, /\.(?:png|jpg|gif|ico|svg)$/)
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
