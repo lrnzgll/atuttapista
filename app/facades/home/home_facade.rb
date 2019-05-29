698925ef18608cc8f6d293d18b9d25c8ce308e9d
lorenzo galli <lrnzgll@gmail.com>
2019-05-29 21:45:35 +0100

    wip

--- app/facades/home/home_facade.rb
+++ app/facades/home/home_facade.rb
@@ -6,7 +6,7 @@ class Home::HomeFacade
     @selected_regions ||= Region.left_joins(:routes).group(:id).order(Arel.sql'COUNT(routes.id) DESC').limit(4).pluck_all(:name, :id)
   end
   def featured_routes
-    @featured_routes ||= Route.order(cached_votes_total: :desc).limit(6).includes(:user).pluck_all(:name, :id, :user_id, :username)
+    @featured_routes ||= Route.includes(:user, :kinds).order(cached_votes_total: :desc).limit(10)
   end
   def routes_by_region
     @routes_by_region ||= Region.order(name: :asc).pluck_all(:name, :id, :slug)
