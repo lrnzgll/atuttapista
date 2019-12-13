76ef56288d8fea5b6faf531914dfd1db9c4fe82a
lorenzo galli <lrnzgll@gmail.com>
2019-12-13 23:39:35 +0000

    add system tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,9 +2,13 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+
+
+# Prevent database truncation if the environment is production
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
