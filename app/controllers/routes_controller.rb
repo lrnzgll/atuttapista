578a20abe11b0cb13485494548590dd0bda3633f
lorenzo galli <lrnzgll@gmail.com>
2019-12-14 12:23:07 +0000

    Fix routes breadcrumb

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -2,10 +2,13 @@
 
 class RoutesController < ApplicationController
   before_action :set_route, only: [:show]
+  before_action :set_first_region, only: [:show]
   before_action :adjust_visit_counter, only: [:show]
   skip_before_action :authenticate_user!, only: [:show]
 
+
   def show
+    breadcrumb @first_region.name, region_path(@first_region.id)
     breadcrumb @route.name, [@route]
     @content = Route::RouteFacade.new(@route)
     authorize @content, policy_class: RoutePolicy
@@ -14,7 +17,11 @@ class RoutesController < ApplicationController
   private
 
   def set_route
-    @route ||= Route.find(params[:id])
+    @route = Route.find(params[:id])
+  end
+
+  def set_first_region
+    @first_region = @route.regions.first
   end
 
   def adjust_visit_counter
